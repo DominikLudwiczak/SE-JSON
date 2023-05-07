f89c7609f3cf263f9be7a29116c14c7fdbbb0385
DominikLudwiczak <dominik.ludwiczak.priv@gmail.com>
2023-05-07 15:15:17 +0200

    base project

---
+++ src/main/java/pl/put/poznan/json/rest/JSONController.java
@@ -0,0 +1,43 @@
+package pl.put.poznan.json.rest;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.*;
+import pl.put.poznan.json.logic.JSON;
+
+import java.util.Arrays;
+
+
+@RestController
+@RequestMapping("/{text}")
+public class JSONController {
+
+    private static final Logger logger = LoggerFactory.getLogger(JSONController.class);
+
+    @RequestMapping(method = RequestMethod.GET, produces = "application/json")
+    public String get(@PathVariable String text,
+                              @RequestParam(value="transforms", defaultValue="upper,escape") String[] transforms) {
+
+        // log the parameters
+        logger.debug(text);
+        logger.debug(Arrays.toString(transforms));
+
+        // perform the transformation, you should run your logic here, below is just a silly example
+        JSON transformer = new JSON(transforms);
+        return transformer.transform(text);
+    }
+
+    @RequestMapping(method = RequestMethod.POST, produces = "application/json")
+    public String post(@PathVariable String text,
+                      @RequestBody String[] transforms) {
+
+        // log the parameters
+        logger.debug(text);
+        logger.debug(Arrays.toString(transforms));
+
+        // perform the transformation, you should run your logic here, below is just a silly example
+        JSON transformer = new JSON(transforms);
+        return transformer.transform(text);
+    }
+}
+
+
