55c216fabd5bf9b1d0764a5567cddfe9b4d24737
eeryczeek <eryk.ptaszynski@vp.pl>
2023-05-14 21:03:24 +0200

    the first iteration to have something to work on

--- src/main/java/pl/put/poznan/json/app/JSONApplication.java
+++ src/main/java/pl/put/poznan/json/app/JSONApplication.java
@@ -3,11 +3,11 @@ package pl.put.poznan.json.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
-@SpringBootApplication(scanBasePackages = {"pl.put.poznan.json.rest"})
+@SpringBootApplication(scanBasePackages = { "pl.put.poznan.json.rest" })
 public class JSONApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(JSONApplication.class, args);
     }
 }
---
+++ src/main/java/pl/put/poznan/json/logic/JSONProcessor.java
@@ -0,0 +1,9 @@
+package pl.put.poznan.json.logic;
+
+class JSONProcessor implements JSONTools {
+    @Override
+    public String processJSON(String jsonData) {
+        // Perform any initial processing on the JSON data
+        return jsonData;
+    }
+}
