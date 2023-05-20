f460e3290f2cce27bfdfad374d4a48a08d4b7395
filo12345678910 <116114032+filo12345678910@users.noreply.github.com>
2023-05-20 17:32:38 +0200

    Created JSON Unminifier class

---
+++ src/main/java/pl/put/poznan/json/logic/JSONUnminifier.java
@@ -0,0 +1,79 @@
+package pl.put.poznan.json.logic;
+
+public class JSONUnminifier extends JSONDecorator {
+    public JSONUnminifier(JSONTools jsonTools) {
+        super(jsonTools);
+    }
+
+    @Override
+    public String processJSON(String jsonData) {
+        // Delegate processing to the underlying component
+        String processedJSON = super.processJSON(jsonData);
+
+        // Unminify the JSON
+        String unminifiedJSON = unminifyJSON(processedJSON);
+
+        return unminifiedJSON;
+    }
+
+    private String unminifyJSON(String minifiedJSON) {
+        StringBuilder unminifiedJSONBuilder = new StringBuilder();
+        int indentationLevel = 0;
+        boolean insideString = false;
+
+        for (int i = 0; i < minifiedJSON.length(); i++) {
+            char currentChar = minifiedJSON.charAt(i);
+
+            if (insideString) {
+                unminifiedJSONBuilder.append(currentChar);
+                if (currentChar == '"') {
+                    // Check if the quote character is escaped
+                    if (minifiedJSON.charAt(i - 1) != '\\') {
+                        insideString = false;
+                    }
+                }
+            } else {
+                switch (currentChar) {
+                    case '{':
+                    case '[':
+                        unminifiedJSONBuilder.append(currentChar);
+                        unminifiedJSONBuilder.append('\n');
+                        indentationLevel++;
+                        appendIndentation(unminifiedJSONBuilder, indentationLevel);
+                        break;
+                    case '}':
+                    case ']':
+                        unminifiedJSONBuilder.append('\n');
+                        indentationLevel--;
+                        appendIndentation(unminifiedJSONBuilder, indentationLevel);
+                        unminifiedJSONBuilder.append(currentChar);
+                        break;
+                    case ',':
+                        unminifiedJSONBuilder.append(currentChar);
+                        unminifiedJSONBuilder.append('\n');
+                        appendIndentation(unminifiedJSONBuilder, indentationLevel);
+                        break;
+                    case ':':
+                        unminifiedJSONBuilder.append(currentChar);
+                        unminifiedJSONBuilder.append(' ');
+                        break;
+                    case '"':
+                        unminifiedJSONBuilder.append(currentChar);
+                        insideString = true;
+                        break;
+                    default:
+                        if (!Character.isWhitespace(currentChar)) {
+                            unminifiedJSONBuilder.append(currentChar);
+                        }
+                        break;
+                }
+            }
+        }
+
+        return unminifiedJSONBuilder.toString();
+    }
+
+    private void appendIndentation(StringBuilder stringBuilder, int indentationLevel) {
+        stringBuilder.append("\t".repeat(Math.max(0, indentationLevel)));
+    }
+}
