b4a3701521e3fef5d8da8ae6172ba625a94ad033
myszjordjot <116115347+myszjordjot@users.noreply.github.com>
2023-05-21 14:41:44 +0200

    Refactor JSONToolsApplication to JSONApplication

--- src/main/java/pl/put/poznan/json/app/JSONApplication.java
+++ src/main/java/pl/put/poznan/json/app/JSONApplication.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication(scanBasePackages = { "pl.put.poznan.json.rest" })
-public class JSONToolsApplication {
+public class JSONApplication {
     public static void main(String[] args) throws IOException {
         System.out.println(":c");
     }
